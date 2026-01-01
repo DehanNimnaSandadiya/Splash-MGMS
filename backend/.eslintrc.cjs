147c55be5f81aeaffa7f36e7dbbdc2b31a9cad0d
dehannimnasandadiya <dehannimnasandadiya@gmail.com>
2026-01-01 14:05:40 +0530

    chore(backend): add ESLint and Prettier configuration

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'max-len': ['error', { code: 100, ignoreUrls: true }],
+  },
+};
+
